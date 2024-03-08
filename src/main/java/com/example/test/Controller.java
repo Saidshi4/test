044fef63541a31d1f51e34340048a0a61414cd53
said4shi <squlizade1@std.beu.edu.az>
2024-03-09 01:20:18 +0400

    rutryty

--- src/main/java/com/example/test/Controller.java
+++ src/main/java/com/example/test/Controller.java
@@ -1,11 +1,13 @@
 package com.example.test;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/v1/api")
+@CrossOrigin(origins = "https://test-three-ebon-73.vercel.app/")
 public class Controller {
     @GetMapping("/test")
     public String test(){
